ef35b0b061dd8e10e2884b21425ea3a16c67287d
johnavinQ^C <johnavinq@gmail.com^C>
2025-03-26 19:50:02 -0500

    hoperfully this morse.c code works this has been hell a little bit but i am smart and can do this please work this time dear god just put in delimiters and string tokens

--- morse.c
+++ morse.c
@@ -32,32 +32,30 @@ const char *char_to_morse(char c)
 void encode_morse(const char *text, char *morse_output, int morse_output_size)
 {
     morse_output[0] = '\0'; // Start with empty string
+    char string[] = '\0'
     int len = 0;
 
     for (int i = 0; text[i] != '\0'; ++i)
     {
         char c = tolower(text[i]);
-
-        if (c == ' ')
-        {
-            // Insert "/ " for word separation
-            if (len + 2 < morse_output_size)
-            {
-                strcat(morse_output, "/");
-                len += 2;
-            }
-            continue;
-        }
-
+        char string[] = '\0'
         const char *morse = char_to_morse(c);
         int morse_len = strlen(morse);
 
         if (morse[0] != '\0' && len + morse_len + 1 < morse_output_size)
         {
-            strcat(morse_output, morse);
-            strcat(morse_output, " ");
+            strcat(string, morse);
+            strcat(string, " ");
             len += morse_len + 1;
         }
+        
+        char* token = strtok(morse_output, "  ");
+        while (token != NULL)
+        {
+                strcat(morse_output, strcat(token,"/"));
+        }
+
+    
         else if (len + morse_len + 1 >= morse_output_size)
         {
             // Stop if buffer limit is reached
@@ -72,4 +70,5 @@ void encode_morse(const char *text, char *morse_output, int morse_output_size)
         morse_output[output_len - 1] = '\0';
     }
 }
+
 
